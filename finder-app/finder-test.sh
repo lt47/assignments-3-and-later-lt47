c081994a032f57651d05011690bbb70c63d6e46c
lt47 <layetee@gmail.com>
2025-09-22 16:46:46 -0500

    Modified finder-test.sh to also include output of the finder command to a specified txt file.

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -60,6 +60,8 @@ done
 
 OUTPUTSTRING=$(${CURR_DIR}/finder.sh "$WRITEDIR" "$WRITESTR")
 
+echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
+
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
