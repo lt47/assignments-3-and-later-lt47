497cce3813b3969d440910f69cfa932a737e7cb0
lt47 <layetee@gmail.com>
2025-10-29 17:11:13 -0500

    adding iotcl command support for iocseekto.

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -16,6 +17,8 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/queue.h>
+
+#include "aesd_ioctl.h" 
 
 #define PORT "9000"
 
@@ -46,10 +49,10 @@ SLIST_HEAD(thread_list, client_thread) head = SLIST_HEAD_INITIALIZER(head);
 
 
 #define LOGI(fmt, ...) syslog(LOG_INFO, fmt, ##__VA_ARGS__)
-#define LOGE(fmt, ...) syslog(LOG_ERR, fmt, ##__VA_ARGS__)
+#define LOGE(fmt, ...) syslog(LOG_ERR,  fmt, ##__VA_ARGS__)
 
 
-void signal_handler(int sig)
+static void signal_handler(int sig)
 {
     stop = 1;
     if (server_fd != -1) {
@@ -60,52 +63,40 @@ void signal_handler(int sig)
 }
 
 
-ssize_t write_all(int fd, const void *buf, size_t len)
+static ssize_t write_all(int fd, const void *buf, size_t len)
 {
     const char *p = buf;
     while (len > 0) {
         ssize_t w = write(fd, p, len);
         if (w < 0) {
-            if (errno == EINTR)
-                continue;
+            if (errno == EINTR) continue;
             return -1;
         }
-        p += w;
-        len -= w;
+        p += (size_t)w;
+        len -= (size_t)w;
     }
     return 0;
 }
 
 
-void append_to_file(const void *buf, size_t len)
+static int send_from_fd(int cfd, int datafd)
 {
-    pthread_mutex_lock(&file_lock);
-    int fd = open(DATAFILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
-    if (fd >= 0) {
-        write_all(fd, buf, len);
-        fsync(fd);
-        close(fd);
+    char buf[BUF_SIZE];
+    for (;;) {
+        ssize_t n = read(datafd, buf, sizeof buf);
+        if (n < 0) {
+            if (errno == EINTR) continue;
+            return -1;
+        }
+        if (n == 0) break;
+        if (write_all(cfd, buf, (size_t)n) < 0) return -1;
     }
-    pthread_mutex_unlock(&file_lock);
-}
-
-void send_file(int cfd)
-{
-    pthread_mutex_lock(&file_lock);
-    int fd = open(DATAFILE, O_RDONLY);
-    if (fd >= 0) {
-        char buf[BUF_SIZE];
-        ssize_t n;
-        while ((n = read(fd, buf, sizeof(buf))) > 0)
-            write_all(cfd, buf, (size_t)n);
-        close(fd);
-    }
-    pthread_mutex_unlock(&file_lock);
+    return 0;
 }
 
 
 #if !USE_AESD_CHAR_DEVICE
-void *timestamp_thread(void *arg)
+static void *timestamp_thread(void *arg)
 {
     (void)arg;
     while (!stop) {
@@ -114,63 +105,160 @@ void *timestamp_thread(void *arg)
 
         time_t now = time(NULL);
         char tbuf[128];
-        struct tm *tm_info = localtime(&now);
-        strftime(tbuf, sizeof(tbuf), "timestamp: %a, %d %b %Y %T %z\n", tm_info);
-        append_to_file(tbuf, strlen(tbuf));
+        struct tm tm_local;
+        localtime_r(&now, &tm_local);
+        strftime(tbuf, sizeof tbuf, "timestamp: %a, %d %b %Y %T %z\n", &tm_local);
+
+        pthread_mutex_lock(&file_lock);
+        int fd = open(DATAFILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        if (fd >= 0) {
+            (void)write_all(fd, tbuf, strlen(tbuf));
+            fsync(fd);
+            close(fd);
+        }
+        pthread_mutex_unlock(&file_lock);
     }
     return NULL;
 }
 #endif
 
 
-void *client_handler(void *arg)
+static int parse_seekto_line(const char *line, size_t len, struct aesd_seekto *seekto)
 {
-    struct client_thread *node = (struct client_thread *)arg;
-    int cfd = node->cfd;
+    static const char prefix[] = "AESDCHAR_IOCSEEKTO:";
+    size_t plen = sizeof(prefix) - 1;
 
-    char buf[BUF_SIZE];
-    ssize_t n;
-    while ((n = recv(cfd, buf, sizeof(buf), 0)) > 0) {
-        append_to_file(buf, n);
-        if (memchr(buf, '\n', n))
-            send_file(cfd);
+    if (len < plen + 3) return 0;
+    if (strncmp(line, prefix, plen) != 0) return 0;
+
+    char tmp[256];
+    size_t cpy = len < sizeof(tmp) - 1 ? len : sizeof(tmp) - 1;
+    memcpy(tmp, line, cpy);
+    tmp[cpy] = '\0';
+
+    unsigned cmd = 0, off = 0;
+    if (sscanf(tmp + plen, "%u,%u", &cmd, &off) == 2) {
+        seekto->write_cmd = cmd;
+        seekto->write_cmd_offset = off;
+        return 1;
+    }
+    return -1;
+}
+
+
+static void *client_handler(void *arg)
+{
+    struct client_thread *node = arg;
+    int cfd = node->cfd;
+    char *acc = NULL;
+    size_t acc_len = 0, acc_cap = 0;
+
+    for (;;) {
+        char buf[BUF_SIZE];
+        ssize_t n = recv(cfd, buf, sizeof buf, 0);
+        if (n == 0) break;
+        if (n < 0) {
+            if (errno == EINTR) continue;
+            LOGE("recv failed: %s", strerror(errno));
+            break;
+        }
+
+        if (acc_len + (size_t)n > acc_cap) {
+            size_t newcap = acc_cap ? acc_cap : 2048;
+            while (acc_len + (size_t)n > newcap) newcap *= 2;
+            char *tmp = realloc(acc, newcap);
+            if (!tmp) { LOGE("realloc failed"); break; }
+            acc = tmp; acc_cap = newcap;
+        }
+        memcpy(acc + acc_len, buf, (size_t)n);
+        acc_len += (size_t)n;
+
+        size_t scan = 0;
+        while (scan < acc_len) {
+            char *nl = memchr(acc + scan, '\n', acc_len - scan);
+            if (!nl) break;
+            size_t line_end = (size_t)(nl - acc) + 1;
+            size_t line_len = line_end - scan;
+
+#if USE_AESD_CHAR_DEVICE
+            pthread_mutex_lock(&file_lock);
+            int datafd = open(DATAFILE, O_RDWR);
+            if (datafd < 0) {
+                pthread_mutex_unlock(&file_lock);
+                LOGE("open(%s): %s", DATAFILE, strerror(errno));
+                goto next;
+            }
+
+            struct aesd_seekto seekto;
+            int parsed = parse_seekto_line(acc + scan, line_len, &seekto);
+            if (parsed == 1) {
+                if (ioctl(datafd, AESDCHAR_IOCSEEKTO, &seekto) != 0)
+                    LOGE("ioctl failed: %s", strerror(errno));
+                send_from_fd(cfd, datafd);
+            } else if (parsed == 0) {
+                write_all(datafd, acc + scan, line_len);
+                lseek(datafd, 0, SEEK_SET);
+                send_from_fd(cfd, datafd);
+            } else {
+                LOGE("Malformed AESDCHAR_IOCSEEKTO");
+            }
+            close(datafd);
+            pthread_mutex_unlock(&file_lock);
+#else
+            pthread_mutex_lock(&file_lock);
+            int fd = open(DATAFILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
+            if (fd >= 0) {
+                write_all(fd, acc + scan, line_len);
+                fsync(fd);
+                close(fd);
+            }
+            fd = open(DATAFILE, O_RDONLY);
+            if (fd >= 0) {
+                send_from_fd(cfd, fd);
+                close(fd);
+            }
+            pthread_mutex_unlock(&file_lock);
+#endif
+        next:
+            scan = line_end;
+        }
+        if (scan > 0) {
+            size_t rem = acc_len - scan;
+            memmove(acc, acc + scan, rem);
+            acc_len = rem;
+        }
     }
 
+    free(acc);
     close(cfd);
     node->done = true;
     return NULL;
 }
 
 
-void daemonize(void)
+static void daemonize(void)
 {
     pid_t pid = fork();
     if (pid < 0) exit(EXIT_FAILURE);
     if (pid > 0) exit(EXIT_SUCCESS);
-
     if (setsid() < 0) exit(EXIT_FAILURE);
     if (chdir("/") != 0) exit(EXIT_FAILURE);
 
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
-
-    int devnull = open("/dev/null", O_RDWR);
-    if (devnull >= 0) {
-        dup2(devnull, STDIN_FILENO);
-        dup2(devnull, STDOUT_FILENO);
-        dup2(devnull, STDERR_FILENO);
-        if (devnull > 2) close(devnull);
+    close(STDIN_FILENO); close(STDOUT_FILENO); close(STDERR_FILENO);
+    int dn = open("/dev/null", O_RDWR);
+    if (dn >= 0) {
+        dup2(dn, STDIN_FILENO);
+        dup2(dn, STDOUT_FILENO);
+        dup2(dn, STDERR_FILENO);
+        if (dn > 2) close(dn);
     }
 }
-
 
 int main(int argc, char *argv[])
 {
     bool daemon = (argc == 2 && strcmp(argv[1], "-d") == 0);
-
     openlog("aesdsocket", LOG_PID, LOG_USER);
-    syslog(LOG_INFO, "aesdsocket starting...");
+    LOGI("aesdsocket starting...");
 
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
@@ -179,38 +267,21 @@ int main(int argc, char *argv[])
     int fd = open(DATAFILE, O_CREAT | O_TRUNC | O_WRONLY, 0644);
     if (fd >= 0) close(fd);
 #endif
-
     if (daemon) daemonize();
 
     struct addrinfo hints = {0}, *res;
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
-
-    if (getaddrinfo(NULL, PORT, &hints, &res) != 0) {
-        syslog(LOG_ERR, "getaddrinfo failed");
-        return EXIT_FAILURE;
-    }
+    if (getaddrinfo(NULL, PORT, &hints, &res) != 0) return EXIT_FAILURE;
 
     server_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (server_fd < 0) {
-        syslog(LOG_ERR, "socket failed");
-        return EXIT_FAILURE;
-    }
-
     int yes = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
-    if (bind(server_fd, res->ai_addr, res->ai_addrlen) < 0) {
-        syslog(LOG_ERR, "bind failed: %s", strerror(errno));
-        return EXIT_FAILURE;
-    }
+    if (bind(server_fd, res->ai_addr, res->ai_addrlen) < 0) return EXIT_FAILURE;
     freeaddrinfo(res);
-
-    if (listen(server_fd, 10) < 0) {
-        syslog(LOG_ERR, "listen failed");
-        return EXIT_FAILURE;
-    }
-    syslog(LOG_INFO, "Listening on port %s", PORT);
+    if (listen(server_fd, 10) < 0) return EXIT_FAILURE;
+    LOGI("Listening on port %s", PORT);
 
 #if !USE_AESD_CHAR_DEVICE
     pthread_t ts_tid;
@@ -218,18 +289,14 @@ int main(int argc, char *argv[])
 #endif
 
     SLIST_INIT(&head);
-
     while (!stop) {
         int cfd = accept(server_fd, NULL, NULL);
         if (cfd < 0) {
             if (errno == EINTR && stop) break;
             continue;
         }
-
-        struct client_thread *node = calloc(1, sizeof(*node));
-        node->cfd = cfd;
-        node->done = false;
-
+        struct client_thread *node = calloc(1, sizeof *node);
+        node->cfd = cfd; node->done = false;
         pthread_create(&node->tid, NULL, client_handler, node);
         SLIST_INSERT_HEAD(&head, node, entries);
 
@@ -257,7 +324,7 @@ int main(int argc, char *argv[])
     unlink(DATAFILE);
 #endif
 
-    syslog(LOG_INFO, "aesdsocket exiting");
+    LOGI("aesdsocket exiting");
     closelog();
     return 0;
-}
+} 
